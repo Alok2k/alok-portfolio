71bc7bb7878cd55e7df6b9f748dd84e9e9faec9b
Alok2k <krishnabhatt1010@gmail.com>
2023-11-20 11:33:44 +0530

    resume addes

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -28,7 +28,7 @@ const HeroSection = () => {
                             </Span>
                         </TextLoop>
                         <SubTitle>{Bio.description}</SubTitle>
-                        <ResumeButton href={Bio.resume} target='display'>Check Resume</ResumeButton>
+                        <ResumeButton href={"https://drive.google.com/file/d/1oX3Z86CRCLcx3i-_9EQoiUHMTyz1rCeg/view?usp=drivesdk"} target='display'>Check Resume</ResumeButton>
                     </HeroLeftContainer>
 
                     <HeroRightContainer id="Right">
